a06c6660bd7c3e070c38b745f31624b46cfda9e5
Veljko <veljko94pesic@gmail.com>
2021-01-28 10:26:46 +0100

    nuxt

---
+++ _nuxt/1617d1e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{374:function(n,t,e){"use strict";e.r(t);var r=e(8),component=Object(r.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container internal"},[this._v("\n  Kasir\n")])}),[],!1,null,"15358457",null);t.default=component.exports}}]);
