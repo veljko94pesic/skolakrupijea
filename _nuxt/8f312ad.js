8355bb14890c6926dcbf51566bf4da204620f620
Veljko <veljko94pesic@gmail.com>
2021-02-09 16:05:37 +0100

    First responsive version

---
+++ _nuxt/8f312ad.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{389:function(n,t,e){"use strict";e.r(t);var c=e(6),component=Object(c.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container internal"},[this._v("\n  Tehnicar\n")])}),[],!1,null,"36bda3eb",null);t.default=component.exports}}]);
